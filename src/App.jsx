5a9f07ecad072d6e351759d535796d914193622e
Kurt Angeles <kurttck7034@gmail.com>
2024-08-18 18:54:12 -0500

    [UPDATE] separe por carpetas y cree buton nav y buton redes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Home from "./Home";
-import Carta from "./Carta";
-import Locales from "./Locales";
+import Home from "./Home/Home";
+import Carta from "./Carta/Carta";
+import Locales from "./Locales/Locales";
 
 function App() {
   return (
